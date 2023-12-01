f5362d13fab202067f40689ccb2e9b70f0cc0824
Nguyen-Bach <nguyenhkin@gmail.com>
2023-12-01 18:07:12 +0700

    asdf

--- MATLAB/changeColor.m
+++ MATLAB/changeColor.m
@@ -1,23 +1,12 @@
-a = imread('coloredChips.png');
-figure(1), imshow(a);
+function [copy] = changeColor(a,x)
 
-% Creat menu of color
-disp('Color selection')
-disp('r for red')
-
-
-% Now we want input from user to tell us which color to change
-x = input(' Please select the color you want to change: ','s');
-%{
 copy = a;
 
 dimension = size(a);
 row_n = dimension (1);
 col_n = dimension (2);  
-%}
 
 
-%{
 switch x
     case 'r'
         for row = 1:row_n
@@ -26,7 +15,7 @@ switch x
                 pixel_green = a(row,col,2);
                 pixel_blue = a(row,col,3);
                 
-                if (pixel_red >= 200) && (pixel_green <= 50) && (pixel_green >= 5)  && (pixel_blue <= 80)
+                if (pixel_red >= 200) && (pixel_green <= 55) && (pixel_green >= 5)  && (pixel_blue >= 10)
                     copy(row,col,1) = 0;
                     copy(row,col,2) = 0;
                     copy(row,col,3) = 0;
@@ -101,7 +90,21 @@ switch x
                 pixel_green = a(row,col,2);
                 pixel_blue = a(row,col,3);
                 
-                if (pixel_red <= 60) && (pixel_green >= 180) && (pixel_blue >= 160)
+                if (pixel_red >= 20)  && (pixel_red <= 60) && (pixel_green >= 130) && (pixel_green <= 220) && (pixel_blue >= 145) && (pixel_blue <= 230);
+                    copy(row,col,1) = 0;
+                    copy(row,col,2) = 0;
+                    copy(row,col,3) = 0;
+                end
+            end
+        end
+     case 'm'
+        for row = 1:row_n
+            for col = 1:col_n
+                pixel_red = a(row,col,1);
+                pixel_green = a(row,col,2);
+                pixel_blue = a(row,col,3);
+                
+                if (pixel_red >= 60) && (pixel_red <= 200) && (pixel_green >= 35) && (pixel_green <= 80) && (pixel_blue >= 130) && (pixel_blue <= 200)
                     copy(row,col,1) = 0;
                     copy(row,col,2) = 0;
                     copy(row,col,3) = 0;
@@ -112,10 +115,6 @@ switch x
     otherwise
         disp('Incorrect selection, please choose from the menu');
 end
-%}
-copy = changeColor(a,x);
-
-% display the modified image
-figure(2),imshow(copy);
 
 
+end
